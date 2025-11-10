822935a6c4689ad281c5d0bf20c297e9873f0a41
giovannirp <giovanni_ge@hotmail.com>
2025-11-10 15:06:53 -0300

    feat: adicionando arquivos

--- lista-tarefas/js/script.js
+++ lista-tarefas/js/script.js
@@ -1,6 +1,13 @@
 // Função em JavaScript
 function adicionar() {
+    // Variáveis
     let tarefa = document.getElementById("tarefa");
+    let lista = document.getElementById("lista");
 
-    console.log(tarefa.value);
+    let item = document.createElement("li");
+    item.textContent = tarefa.value;
+
+    lista.appendChild(item);
+
+    tarefa.value = "";
 }
