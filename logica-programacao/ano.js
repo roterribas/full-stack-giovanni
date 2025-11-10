6fde7f83f88d54e355276275888b2c861db1b554
giovannirp <giovanni_ge@hotmail.com>
2025-11-10 16:22:52 -0300

    feat: adicionando logica copa e ano

---
+++ logica-programacao/ano.js
@@ -0,0 +1,12 @@
+    // Criar um programa, para verificar o ano de nascimento
+    const anoAtual = 2025;
+
+    /*
+        Pegar o Ano atual e subtrair pela idade
+    */
+
+    document.write("Eu nasci em: " + (anoAtual - 37));
+
+    // Diego, 35 anos
+    // Juliana, 10 anos
+    // João, 17 anos
---
+++ logica-programacao/copa.js
@@ -0,0 +1,9 @@
+// Descobrir o ano da próxima Copa do Mundo
+const anoAtual = 2025;
+
+/*
+    A Copa do Mundo acontece a cada 4 anos
+    A última foi em 2022
+*/
+
+document.write("A próxima Copa do Mundo será em: " + (anoAtual + 1));
