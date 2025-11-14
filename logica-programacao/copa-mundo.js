dbfb949278ebad28695a34235e7a28ff9463ab42
giovannirp <giovanni_ge@hotmail.com>
2025-11-14 16:10:59 -0300

    feat: add

--- logica-programacao/copa-mundo.js
+++ logica-programacao/copa-mundo.js
@@ -17,3 +17,8 @@
 
 let anoInicio = Number(prompt("Digite o ano da primeira Copa do Mundo que você viu:"));
 let anoAtual = 2025;
+
+while(anoInicio <= anoAtual) {
+    document.write("Copa do mundo: " + anoInicio + "<br>");
+    anoInicio = anoInicio + 4;
+}
