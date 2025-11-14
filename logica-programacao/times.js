d53195c62c23ef82c0f901dbe2b509293e991b92
giovannirp <giovanni_ge@hotmail.com>
2025-11-14 15:01:40 -0300

    adicionando novas logicas

---
+++ logica-programacao/times.js
@@ -0,0 +1,14 @@
+/*
+Crie esse array de times 
+const times = ["Botafogo", "São Paulo", "Paulista"];
+
+Usando for loop, mostre na tela, a messagem seguinte:
+- O time é: Botafogo
+- O time é: São Paulo
+- O time é: Paulista
+*/
+const times = ["Botafogo", "São Paulo", "Paulista", "Bangu"];
+
+for (let i = 0; i < times.length; i++) {
+    console.log(`- O time é: ${times[i]}`)
+}
