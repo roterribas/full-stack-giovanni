742f3516321beb2b23ee213acc918013e23a2e17
giovannirp <giovanni_ge@hotmail.com>
2025-11-18 16:14:43 -0300

    feat: add

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -1,11 +1,34 @@
+const form = document.querySelector("form");
+const lista = document.querySelector(".lista");
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
-// inputEmail
-// inputTel
+const inputTel = document.getElementById("telefone");
 
-
-function btnCadastrar(event) {
+form.addEventListener("submit", function(event) {
     event.preventDefault();
-    console.log(inputNome.value);
-    console.log(inputEmail.value);
-}
+
+    // Validação do formulário
+    if (inputNome.value == "" || inputEmail.value == "" || inputTel.value == "") {
+        alert("Digite seus dados");
+        return false;
+    }
+
+    // Criar LI
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+        <span class="contato-nome">${inputNome.value}</span>
+        <span class="contato-email">${inputEmail.value}</span>
+        <span class="contato-telefone">${inputTel.value}</span>
+    `;
+
+    console.log(li)
+
+
+    // appendChild()
+    lista.appendChild(li)
+
+    // Limpar inputs
+    form.reset();
+
+})
