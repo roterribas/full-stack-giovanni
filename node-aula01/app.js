9cd273676e64e5a37e8f0c856141a303e733b036
giovannirp <giovanni_ge@hotmail.com>
2025-11-24 16:20:33 -0300

    feat: add

--- node-aula01/app.js
+++ node-aula01/app.js
@@ -2,10 +2,28 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Variável com os dados produtos
+const produtos = [
+    { id: 1, nome: 'Produto A', preco: 100 },
+    { id: 2, nome: 'Produto B', preco: 150 },
+    { id: 3, nome: 'Produto C', preco: 200 },
+];
+
 // Criar função
 // Rota principal
 app.get('/', (req, res) => {
     res.send('Bem vindo ao servidor Node.js');
+});
+
+// Rota produtos
+app.get('/produtos', (req, res) => {
+    res.send(produtos);
+});
+
+// Rota home
+app.get('/home', (req, res) => {
+    // console.log("olá: ", __dirname);
+    res.sendFile(__dirname + '/public/home.html');
 });
 
 // Executando o servidor
